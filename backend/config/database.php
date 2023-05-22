2257c259a7cc0f5c80dba9d42788ea762cf7ceea
IgroGood <IgroGood@mail.ru>
2023-05-22 14:30:42 +1200

    Лара + монго

--- backend/config/database.php
+++ backend/config/database.php
@@ -93,13 +93,13 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE', 'homestead'),
             'username' => env('DB_USERNAME', 'homestead'),
             'password' => env('DB_PASSWORD', 'secret'),
             'options' => [
-                'appname' => 'homestead',
+                'appname' => 'tours',
             ],
 
     ],
