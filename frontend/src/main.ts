cfd087c61d8d60eca0a777b0ee6bcf7b245f0f02
IgroGood <IgroGood@mail.ru>
2023-05-23 10:08:15 +1200

    add theme

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+
+
+import PrimeVue from 'primevue/config'
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
