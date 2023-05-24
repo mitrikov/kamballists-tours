048aef3caa64ecc1469db3ff45341dbdcfe71882
IgroGood <IgroGood@mail.ru>
2023-05-24 16:15:14 +1200

    карта и типа верстка

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import ItemYandexMap from '@/components/ItemYandexMap.vue'
 </script>
 
 <template>
   <header>
     <nav class="menu">
-      <div class="menu-logo"></div>
-      <ul class="menu-left">
-        <li><a href="#">Меню</a></li>
-        <li><a href="#">Поиск</a></li>
-        <li><a href="#">Бонусы</a></li>
-      </ul>
+      <div class="menu-left">
+        <div class="menu-logo"></div>
+        <ul class="menu-left__list">
+          <li><a href="#">Меню</a></li>
+          <li><a href="#">Поиск</a></li>
+          <li><a href="#">Бонусы</a></li>
+        </ul>
+      </div>
 
-      <ul class="menu-right">
+      <ul class="menu-right__list">
         <li><a href="#">РУС / RUB</a></li>
         <li><a href="#">Поддержка</a></li>
         <li><a href="#">Избранное</a></li>
@@ -20,9 +23,16 @@ import { RouterLink, RouterView } from 'vue-router'
       </ul>
     </nav>
   </header>
+  <ItemYandexMap />
   <RouterView />
 </template>
 
 <style lang="sass" scoped>
-
+.menu
+  display: flex
+  justify-content: space-between
+  &-left__list
+    display: flex
+  &-right__list
+    display: flex
 </style>
---
+++ frontend/src/components/ItemYandexMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="map" class="yandex-maps"></div>
+</template>
+
+<script setup>
+const API_KEY = '83914ce8-5042-494d-a186-d5948174283e'
+const script = document.createElement('script')
+script.id = 'yandexMap'
+script.src = `https://api-maps.yandex.ru/2.1/?apikey=${API_KEY}&lang=ru_RU`
+document.head.appendChild(script)
+
+function init(){
+  var myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 7
+  });
+}
+
+script.onload = () => {
+  ymaps.ready(init)
+}
+
+</script>
+
+<style lang="sass" scoped>
+.yandex-maps
+  width: 400px
+  height: 400px
+  border: 1px solid red
+</style>
