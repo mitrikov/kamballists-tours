29695738b3f5e0d3749ab3930414d4ac0717981a
mitrikov <j6>
2023-05-29 02:55:11 +1200

    imports

---
+++ db/mongo-seed/import.sh
@@ -0,0 +1,1 @@
+ls -1 /mongo-seed/*.json | while read jsonfile; do mongoimport --host tours-mongo --db tours --authenticationDatabase admin -u admin -p admin --type json --file $jsonfile --jsonArray; done 
